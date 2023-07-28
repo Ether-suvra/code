fd463d1e0140da45016fc89828f4660ec0e5d85f
Ether-suvra <suvrabhattacharjee88@gmail.com>
2023-07-28 17:19:14 +0530

    Day1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,40 @@
 package org.example;
 
+import java.util.Scanner;
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner input = new Scanner(System.in);
+        int N = input.nextInt();
+        int[] arr = new int[N];
+        // taking array inputs
+        for (int i = 0; i < N; i++) {
+            arr[i] = input.nextInt();
+        }
+        selectionSort(arr);
+       // printing the array
+        System.out.println("After selection sort:");
+        System.out.println(Arrays.toString(arr));
     }
-}
+
+    public static void selectionSort(int[] arr) {
+        int N = arr.length;
+
+        for (int i = 0; i < N - 1; i++) {
+            int maxIndex = i;
+            for (int j = i + 1; j < N; j++) {
+                if (arr[j] > arr[maxIndex]) {
+                    maxIndex = j;
+                }
+            }
+
+            // Swap the largest element with the current element
+            int temp = arr[i];
+            arr[i] = arr[maxIndex];
+            arr[maxIndex] = temp;
+        }
+    }
+
+    }
+
